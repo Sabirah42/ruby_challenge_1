ff25bd7763c461a0b33c227b3de9f60827f06987
Sabirah <sabirah.miah.ahammed@gmail.com>
2022-12-01 17:02:23 +0000

    First Commit

---
+++ password_validator.rb
@@ -0,0 +1,7 @@
+# Valid passowords are
+# - 8 or more characters
+# - include at least one of these characters: !, @, $, %, &
+
+def valid?(password)
+    return password.length >= 8 && password.match?(/[!@$%&]/)
+end
